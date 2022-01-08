5c7cf96b2b7295a48980dd636cc571e8ab6c56a1
Erick Veríssimo <erickandradesilva@hotmail.com>
2022-01-08 12:30:53 +0000

    Adição do exemplo de lógicas frontend para rodar o projeto

---
+++ Base-Frontend/static/frontend.js
@@ -0,0 +1,32 @@
+if (typeof kotlin === 'undefined') {
+  throw new Error("Error loading module 'frontend'. Its dependency 'kotlin' was not found. Please, check whether 'kotlin' is loaded prior to 'frontend'.");
+}var frontend = function (_, Kotlin) {
+  'use strict';
+  var Unit = Kotlin.kotlin.Unit;
+  function limpar() {
+    var elem = document.getElementById('p1');
+    if (elem != null) {
+      elem.innerHTML = '';
+    }}
+  function botaoClicado(nome) {
+    var elem = document.getElementById('p1');
+    if (elem != null) {
+      elem.innerHTML = elem.innerHTML + ('\n' + '            <br> <font color=' + '"' + nome + '"' + '>Este texto foi inserido pelo programa Kotlin ' + '\n' + '            ao pressionar o botao ' + nome + '<\/font>' + '\n' + '            <button onclick=' + '"' + "frontend.botaoClicado('yellow')" + '"' + '>Nao aperte esse botao<\/button>' + '\n' + '            ');
+    }}
+  function main$lambda() {
+    var elem = document.getElementById('p1');
+    if (elem != null) {
+      elem.innerHTML = elem.innerHTML + '<br> Tick\n';
+    }return Unit;
+  }
+  function main() {
+    for (var c = 1; c <= 100; c++)
+      window.setInterval(main$lambda, 1000);
+  }
+  _.botaoLimpar = limpar;
+  _.botaoClicado = botaoClicado;
+  _.main = main;
+  main();
+  Kotlin.defineModule('frontend', _);
+  return _;
+}(typeof frontend === 'undefined' ? {} : frontend, kotlin);
